6288a5ef007aca9e301ce297057fc34871645a3a
Mahendra Singh Tomar <mahendra2022tomar@gmail.com>
2023-04-11 01:59:58 -0600

    description

--- README.md
+++ README.md
@@ -3,8 +3,25 @@ Time to access symbolic control input via [Scarab](https://github.com/kofyou/sca
 
 
 
-Thanks to [Mingsheng Xu](https://kofyou.github.io) for CMakeLists.txt which is used to run start/stop API of DynamoRio.
+Thanks to [Mingsheng Xu](https://kofyou.github.io) for [CMakeLists.txt](files/CMakeLists.txt) which is used to run start/stop API of DynamoRio.
 
-* For a 3 dimensional kinematic vehicle model, symbolic controller for reachability designed using [SCOTSv0.2](https://github.com/mahendrasinghtomar/SCOTSv0.2_Copy). 
-* SCOTSv0.2 saves symbolic controller, in the form of a lookup table, in a '.scs' file. To get a controller of size greater than 1 GB, multiple adjacent scenarios with randomised location of obstacles are created via [vehicle.cc](). The number of scenarios is controlled by ...
-* ...
+## For a 3 dimensional kinematic vehicle model, symbolic controller for reachability computed using [SCOTSv0.2](https://github.com/mahendrasinghtomar/SCOTSv0.2_Copy):
+* SCOTSv0.2 saves symbolic controller, in the form of a lookup table, in a '.scs' file. To get a controller of size greater than 1 GB, multiple adjacent scenarios with randomised location of obstacles created. 
+* Accessing control input from a symbolic controller:
+![Accessing control input from a symbolic controller](figs/symbolicControlAccess.jpg)
+The memory trace for the symbolic-control-input-access is obtained by:
+```
+      dr_app_setup();
+      dr_app_start();
+      std::vector<input_type> u = conVec[i].get_control<state_type,input_type>(x);
+      dr_app_stop_and_cleanup();
+```
+
+<!-- * For a selected vehicle scenario composed of 12 scenes, each with a controller of size ~95 MB -->
+* The below figure shows the arena: ![arena](figs/vehicle_12_scenes.jpg) with obstacles in color blue, the target area in red, and a trajectory in color black.
+The controller is obtained by considering the arena to be composed of 12 adjacent scenes, each with a [controller](files/vehicle_controller_msa_0.scs) of size ~96 MB. 
+The trajectory for initial state x0 = {.5, 0.4, 0} is [simulated](files/simulate4.cpp) and the target set is reached in 971 steps.
+* Amazon [Deepracer](https://www.amazon.com/dp/B07JMHRKQG?th=1) involves Intel(R) Atom(TM) Processor E3930, which is based on the [Goldmont](https://en.wikichip.org/wiki/intel/microarchitectures/goldmont) microarchitecture. 
+The parameter file for [KabyLake](https://github.com/kofyou/scarab/blob/master/src/PARAMS.kaby_lake) is edited to match the available data for Deepracer, and is named [PARAMS.GoldmontV01](files/PARAMS.GoldmontV01).
+* During [trajectory simulation](files/simulate4.cpp), for each cycle of control access, a separate trace file generated. The EXECUTION_TIME of a trace in Scarab, with [PARAMS.GoldmontV01](files/PARAMS.GoldmontV01), is taken to be the access time of symbolic control. The plot of time over 971 steps is: ![SymbolicControlAccessTime971steps](figs/SymbolicControlAccessTime971steps.jpg) The time lies in the range **`13.58921718 to 13.88921688 microsecond`**.
+* With [PARAMS.kaby_lake](https://github.com/kofyou/scarab/blob/master/src/PARAMS.kaby_lake) the access time of symbolic control is in the range **`7.61875 to 7.67875 microsecond`**.
---
+++ files/simulate4.cpp
@@ -0,0 +1,151 @@
+/* dr_api
+ * simulate4.cc, stitched
+ *
+ */
+#include "dr_api.h"
+#include <assert.h>
+#include <iostream>
+#include <array>
+#include <cmath>
+
+/* SCOTS header */
+#include "scots.hh"
+/* ode solver */
+#include "RungeKutta4.hh"
+/* time profiling */
+#include "TicToc.hh"
+
+// #define TRDIR "400"
+/* state space dim */
+const int state_dim=3;
+/* input space dim */
+const int input_dim=2;
+/* sampling time */
+const double tau = 0.3;
+
+std::string FILENAMEMODIF = "_msa";
+const std::string EXAMPLE = "vehicle";
+
+/*
+ * data types for the state space elements and input space
+ * elements used in uniform grid and ode solvers
+ */
+using state_type = std::array<double,state_dim>;
+using input_type = std::array<double,input_dim>;
+
+/* we integrate the vehicle ode by tau sec (the result is stored in x)  */
+auto  vehicle_post = [](state_type &x, const input_type &u) {
+  /* the ode describing the vehicle */
+  auto rhs =[](state_type& xx,  const state_type &x, const input_type &u) {
+    double alpha=std::atan(std::tan(u[1])/2.0);
+    xx[0] = u[0]*std::cos(alpha+x[2])/std::cos(alpha);
+    xx[1] = u[0]*std::sin(alpha+x[2])/std::cos(alpha);
+    xx[2] = u[0]*std::tan(u[1]);
+  };
+  /* simulate (use 10 intermediate steps in the ode solver) */
+  scots::runge_kutta_fixed4(rhs,x,u,state_dim,tau,10);
+};
+
+#define TRDIR "GoldmontV01_"
+
+bool
+my_setenv(const char *var, const char *value)
+{
+#ifdef UNIX
+    return setenv(var, value, 1 /*override*/) == 0;
+#else
+    return SetEnvironmentVariable(var, value) == TRUE;
+#endif
+}
+
+int main() {
+
+  double xshift = 9.5;
+  int si = 11;  // (si+1) adjacent scenes
+
+  //  if (!my_setenv("DYNAMORIO_OPTIONS",
+  //                  "-stderr_mask 0xc -rstats_to_stderr "
+  //                  "-client_lib ';;-offline -outdir /mnt/c/Users/mahen/StorageLocal/rough/SCOTSv0.2_Copy/examples/vehicle/build/memtrace_s3_F2_edit'"))
+  //       std::cerr << "failed to set env var!\n";
+
+  std::string setenvString = "-stderr_mask 0xc -rstats_to_stderr -client_lib ';;-offline -outdir /mnt/c/Users/mahen/StorageLocal/rough/SCOTSv0.2_Copy/examples/vehicle_mxu/build/memtrace_s3_F2_" + std::string(TRDIR) + "'";
+
+  if (!my_setenv("DYNAMORIO_OPTIONS",  
+                    setenvString.c_str()))
+        std::cerr << "failed to set env var!\n";
+
+TicToc run_time;
+run_time.tic();
+
+   /* define function to check if we are in target */
+  auto target = [&si,&xshift](const state_type& x, int& i) {
+    /* i updated */
+    // if (9 <= x[0] && x[0] <= 9.5 && 0 <= x[1] && x[1] <= 0.5)
+    if (9.5+(i*xshift) <= x[0] && x[0] <= 10+(i*xshift) && 0 <= x[1] && x[1] <= 1){
+      if(i==si)
+        return true;
+      else
+        i++;
+    }
+    return false;
+  };
+
+// scarab_begin(); // cycles to read static controller, together with 'if' check and 'con' creation.
+  // std::vector<scots::StaticController> conVec[si+1];
+  std::vector<scots::StaticController> conVec;
+  for(int i=0;i<si+1;i++){
+    scots::StaticController conTemp;
+    /* read controller from file */
+    // scots::StaticController con;
+    if(!read_from_file(conTemp,EXAMPLE+"_controller"+FILENAMEMODIF+"_"+std::to_string(i))) {
+      std::cout << "Could not read controller from controller.scs\n";
+      return 0;
+    }
+    // conVec[i] = conTemp;
+    conVec.push_back(conTemp);
+  }
+// scarab_end();
+  
+  std::cout << "\nSimulation:\n " << std::endl;
+
+  // dr_app_setup();
+  // assert(!dr_app_running_under_dynamorio());
+
+  // cycles to read static controller, together with 'if' check and 'con' creation.
+  // for(int ii=0;ii<5;ii++){
+    int no_steps = 0;
+    state_type x={{.5, 0.4, 0}};
+    int i=0;
+    while(1) {
+      no_steps++;
+
+      // if(no_steps==5)
+      //   return 0;
+
+      dr_app_setup();
+      dr_app_start();
+      // assert(dr_app_running_under_dynamorio());
+      std::vector<input_type> u = conVec[i].get_control<state_type,input_type>(x);
+      dr_app_stop_and_cleanup();
+      // assert(!dr_app_running_under_dynamorio());
+
+      std::cout << x[0] <<  " "  << x[1] << " " << x[2] << ", scene " << i <<  "\n";
+      //std::cout << u[0][0] <<  " "  << u[0][1] << "\n";
+      vehicle_post(x,u[0]);
+      if(target(x,i)) {
+        std::cout << "Arrived: " << x[0] <<  " "  << x[1] << " " << x[2] << std::endl;
+        break;
+      }
+    }
+    
+    std::cout << "Vehicle Run time = ";
+    run_time.toc();
+    std::cout << " Number of steps = " << no_steps << "\n" ;
+
+    // dr_app_stop_and_cleanup();
+    // assert(!dr_app_running_under_dynamorio());
+
+  // }
+
+  return 0;
+}
